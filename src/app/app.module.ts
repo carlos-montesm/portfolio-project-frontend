1a6be54a1aa2edcdb81c09ba77a3dc6c9576efb6
Carlos Montes <carlos.montes88@gmail.com>
2023-08-08 16:30:34 -0500

    Four components were created, each one for a different page with the following categories: aboutme, projects, create, contact, and added in the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
