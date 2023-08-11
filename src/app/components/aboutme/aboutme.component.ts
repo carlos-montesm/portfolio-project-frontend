58229a0057f6b1c6f513de16705e136e8eb78289
Carlos Montes <carlos.montes88@gmail.com>
2023-08-11 17:35:07 -0500

    Styles are placed on the about me page, to the content of the titles, subtitles, paragraphs, the template for this about me component is also created, and its main attributes are placed

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
   styleUrls: ['./aboutme.component.css']
 })
-export class AboutmeComponent {
+export class AboutmeComponent implements OnInit {
+  /* This is the title, subtitle and email of the 
+  about me page, which shows my name, job and email */
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
+  constructor(){
+    this.title = "Carlos Montes";
+    this.subtitle = "Freelancer Frontend Developer";
+    this.email = "test-email@test.com";
+  }
+
+  ngOnInit(): void {
+  }
 }
