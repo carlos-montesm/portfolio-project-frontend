b5508eea2f4d344e96e6772def9d195908cbd376
Carlos Montes <carlos.montes88@gmail.com>
2023-08-26 00:48:19 -0500

    feat: change upload an image to the created project, in the onSubmit() method

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/app/models/project';
-import { ProjectService } from 'src/app/services/project.service';
+import { ProjectService } from 'src/app/services/project.service'; // Service to save projects in the database
+import { UploadService } from 'src/app/services/upload.service'; // Service to save/upload images on the server
+import { Global } from 'src/app/services/global'; // Global configuration file, api url
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title: string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _projectService: ProjectService
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
   ){
     this.title = "Create project";
     this.project = new Project('','','','',0,'','');
     this.status = "";
+    this.filesToUpload = [];
   }
 
   ngOnInit(): void {
@@ -34,8 +39,17 @@ export class CreateComponent implements OnInit {
     this._projectService.saveProject(this.project).subscribe(
       response => {
         if(response.project){
-          this.status = 'success'; // Project created
-          form.reset(); // Empty all form fields
+          
+          // Upload the image
+          this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image')
+          .then((result:any) => {
+            
+            console.log(result);
+
+            this.status = 'success'; // Project created
+            form.reset(); // Empty all form fields
+          });
+          
         }else{
           this.status = 'failed'; // Project not created
         }
